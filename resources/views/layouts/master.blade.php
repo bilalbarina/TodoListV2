d6caacc78b4d4baef71f956863e9348ec15ad855
bilalbarina <dev.barina@gmail.com>
2022-11-15 17:01:33 +0100

    ReactJS

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Todo List </title>
+    <script src="{{ asset('assets/js/react.development.js') }}"></script>
+    <script src="{{ asset('assets/js/react-dom.development.js') }}"></script>
+    <script src="{{ asset('assets/js/babel.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery-3.6.1.min.js') }}"></script>
+</head>
+<body>
+    <div id="app"></div>
+    <script src="{{ asset('app/Index.js') }}" type="text/babel"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return response()->json(['success' => true]);
-});
+Route::view('/', 'index');
