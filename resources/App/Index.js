713a74f18ad86eb9632b7523627750e649acd9bb
bilalbarina <dev.barina@gmail.com>
2022-11-21 09:13:33 +0100

    TP Standard

--- resources/App/Index.js
+++ resources/App/Index.js
@@ -1,7 +1,10 @@
-class Index extends React.Component {
+class Tasks extends React.Component {
     constructor(props) {
         super(props);
         this.state = { tasks: [] };
+
+        this.Task = this.Task.bind(this)
+        this.deleteTask = this.deleteTask.bind(this)
     }
 
     componentDidMount() {
@@ -12,66 +15,115 @@ class Index extends React.Component {
         });
     }
 
+    deleteTask(taskId) {
+        $.ajax({
+            url: `/api/task/${taskId}`,
+            type: "DELETE",
+            success: () => window.location.reload(false),
+        });
+    }
+
+    Task({ task }) {
+        return (
+            <li>
+            <div>
+                <span> {task.title} - </span>
+                <button onClick={() => this.deleteTask(task.id)}> Delete </button>
+            </div>
+            </li>
+        );
+    }
+
     render() {
-        return [
-            <CreateTaskForm />,
-            this.state.tasks.map((task) => <Task title={task.title} />)
-        ];
+        return (
+            <>
+                <Form />
+                <br />
+                <ul>
+                    {this.state.tasks.map((task, index) => (
+                        <this.Task key={index} task={task} />
+                    ))}
+                </ul>
+            </>
+        );
     }
 }
 
-const Task = (props) => {
-    return (
-            <li>
-                { props.title }
-            </li>
-    );
-}
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
 
-const CreateTaskForm = () => {
-    const refs = {
-        title: React.useRef(),
-        description: React.useRef(),
-    };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    const submit = (event) => {
-        event.preventDefault();
+    handleSubmit(e) {
+        e.preventDefault();
 
-        const task = {
-            title: refs.title.current.value,
-            description: refs.description.current.value,
+        const formData = new FormData(e.target);
+
+        let task = {
+            title: formData.get("title"),
+            description: formData.get("description"),
         };
 
         $.ajax({
             url: "/api/task/create",
             method: "POST",
             data: task,
-            success: (data) => console.log(data),
+            success: () => window.location.reload(false),
         });
+    }
 
-        window.location.reload(false);
-    };
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="title" placeholder="Title" />
+                <br />
+                <input
+                    type="text"
+                    name="description"
+                    placeholder="Description"
+                />
+                <br />
+                <button type="submit"> Create </button>
+            </form>
+        );
+    }
+}
 
-    return (
-        <form>
-            <input
-                type="text"
-                name="name"
-                placeholder="Title"
-                ref={refs.title}
-            />
-            <br/>
-            <input
-                type="text"
-                name="description"
-                placeholder="Description"
-                ref={refs.description}
-            />
-            <br/>
-            <button onClick={submit}> Create </button>
-        </form>
+const Index = () => (
+    <Tasks/>
     );
-};
+// class TestForm extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+
+//     handleSubmit = (e) => {
+//         e.preventDefault();
+
+//         let formData = new FormData(e.target);
+//         console.log('Name: ' + formData.get('name'))
+//         console.log('Description: ' + formData.get('description'))
+//     };
+
+//     render() {
+//         return (
+//             <form onSubmit={this.handleSubmit}>
+//                 <input type="text" name="name" placeholder="Title" />
+//                 <br />
+//                 <input
+//                     type="text"
+//                     name="description"
+//                     placeholder="Description"
+//                 />
+//                 <br />
+//                 <button type="submit"> Submit </button>
+//             </form>
+//         );
+//     }
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<Index />);
